8e7a492f1e4f108f29768cc7d6002126f4226433
root <root@Galaxy-Book-Noam>
2024-03-26 14:56:10 +0100

    Ex_2_2

--- Ex_2/build_and_run.sh
+++ Ex_2/build_and_run.sh
@@ -2,11 +2,10 @@
 
 # Compilation
 g++ -std=c++20 -fmodules-ts -c animals.cpp -o animals.o
-g++ -std=c++20 -fmodules-ts -c human.cpp -o human.o
 g++ -std=c++20 -fmodules-ts -c main.cpp -o main.o
 
 # Édition de liens
-g++ -std=c++20 -fmodules-ts animals.o human.o main.o -o executable
+g++ -std=c++20 -fmodules-ts animals.o main.o -o executable
 
 # Exécution
 ./executable
